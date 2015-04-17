41d7793b00e6fa9bb2c246ed18b119d38fdc36cb
romale23 <romale23@gmail.com>
2015-04-17 14:02:55 -0500

    test

---
+++ ExideXChangeNavigation/Features/XChangeCustomSiteMapProvder/XChangeCustomSiteMapProvder.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="2c7e7dc7-b300-498b-916a-df38980e3081" featureId="2c7e7dc7-b300-498b-916a-df38980e3081" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ExideXChangeNavigation Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="2242d0f4-fc6d-42c2-8a42-1bb7824a03f5" />
+  </projectItems>
+</feature>
---
+++ ExideXChangeNavigation/XChangeCustomSiteMapProvder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web;  
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Publishing;
+using Microsoft.SharePoint.Publishing.Navigation;
+
+
+
+namespace ExideXChangeNavigation
+{
+    public class XChangeCustomSiteMapProvder : PortalSiteMapProvider
+    {
+        public override SiteMapNodeCollection GetChildNodes(SiteMapNode node)
+        {
+            PortalSiteMapNode pNode = node as PortalSiteMapNode;
+            if (pNode != null)
+            {
+                if (pNode.Type == NodeTypes.Area && pNode.WebId == SPContext.Current.Site.RootWeb.ID)
+                {
+                    SiteMapNodeCollection nodeColl = base.GetChildNodes(pNode);
+                    SiteMapNode childNode = new SiteMapNode(this,
+                                                            "&lt;http://www.google.com&gt;", "http://www.google.com",
+                                                            "Google");
+
+                    SiteMapNode childNode1 = new SiteMapNode(this,
+                                                             "&lt;http://www.bing.com&gt;",
+                                                             "http://www.bing.com", "Bing");
+                    SiteMapNode childNode2 = new SiteMapNode(this,
+                                                             "&lt;http://www.hardocp.com&gt;",
+                                                             "http://www.hardocp.com", "hardocp");
+
+                    nodeColl.Add(childNode);
+
+                    SiteMapNodeCollection test = new SiteMapNodeCollection();
+                    test.Add(childNode1);
+                    test.Add(childNode2);
+                    childNode.ChildNodes = test;
+                    
+
+                    
+                    return nodeColl;
+                }
+                return base.GetChildNodes(pNode);
+            }
+            return new SiteMapNodeCollection();
+        }
+    }
+}
